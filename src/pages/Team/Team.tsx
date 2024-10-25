fda195b42dd307aeba698fe987bc44a3d9cf4389
Andy-Ansong <andy.ansong@amalitechtraining.org>
2024-10-25 16:49:23 +0000

    style team page

--- src/pages/Team/Team.tsx
+++ src/pages/Team/Team.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import './styles.css'
+import { CiMail } from 'react-icons/ci'
 import { Employee } from '../../model/Employee'
+import { FiPhoneCall } from 'react-icons/fi'
 import axios from 'axios'
 
 const TeamPage: React.FC = () => {
@@ -9,7 +11,7 @@ const TeamPage: React.FC = () => {
   useEffect(() => {
     axios.get(`http://localhost:3030/api/v1/employees/current/team`,
       {
-        headers: {Authorization: `Bearer ${localStorage.getItem("token")}`},
+        headers: {Authorization: `Bearer ${localStorage.getItem('token')}`},
         withCredentials: true
       })
       .then(res => {
@@ -22,24 +24,40 @@ const TeamPage: React.FC = () => {
   }, [])
 
   return (
-    <div className="team-page">
+    <div className='team-page'>
       <h1>{teamMembers[0]?.Department.Team.name}</h1>
+      <h2>{teamMembers.length} {teamMembers.length === 1 ? `Member` : `Members`}</h2>
+      <div className='team-grid'>
       {
         teamMembers.map((member, index) => (
-          <div key={index} className="department">
-            <h2>{member.name}</h2>
-            <div className="employee-grid">
-              <img src={member.image} alt={member.name} className="member-image" />
-              <h3>{member.Department.Team.name}</h3>
-              <p>{member.Department.Role.position}</p>
-                <div className="member-details">
-                  <p><strong>Email:</strong> {member.email}</p>
-                  <p><strong>Phone:</strong> {member.phoneNumber}</p>
+          <div key={index} className='team-card'>
+            <div className='image-container'>
+              <img src={member.image} alt={member.name} className='member-image' />
+            </div>
+            <div className='personal-info'>
+              <h2>{member.name}</h2>
+              <h3>{member.Department.Team.role}</h3>
+            </div>
+            <div className='member-details'>
+              <h3>{member.Department.Role.position}</h3>
+              <h4>{member.Department.Team.role}</h4>
+              <h3>Date Hired</h3>
+              <h4>{member.Department.Role.startDate.slice(0,10)}</h4>
+              <div className='member-contacts'>
+                <div>
+                  <CiMail />
+                  <a href={`mailto:${member.email}`}>{member.email}</a>
                 </div>
+                <div>
+                  <FiPhoneCall />
+                  <a href={`tel:${member.phoneNumber}`}>{member.phoneNumber}</a>
+                </div>
+              </div>
             </div>
           </div>
         ))
       }
+      </div>
     </div>
   )
 }
